35e45bd656d2f5e0c3ac1a45a6dafbed09707c54
Samantha Heisterkamp <samanthapaige912@yahoo.com>
2022-06-24 16:10:23 -0600

    initial commit

---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.foodtruck.entities;
+// does not have a main
+public class FoodTruck {
+	//You will define a FoodTruck class with fields for a 
+	//unique numeric id, a name ("TacoRific", "Mediterranean Medic", etc.), 
+	//food type ("Tacos", "Falafel", etc.), and a numeric rating.
+}
+//have an array of up to five food trucks.
