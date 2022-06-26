c7f7c273cf3a01762c3eacc07bd64d8c21dafb9e
Samantha Heisterkamp <samanthapaige912@yahoo.com>
2022-06-26 13:53:50 -0600

    FoodTruck.java is done git add .

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,13 +1,42 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
 import com.skilldistillery.foodtruck.entities.FoodTruck;
 
+
 public class FoodTruckApp {
-	
-	private FoodTruck fleetOfFoodTrucks [];
+
+	private FoodTruck fleetOfFoodTrucks[]; // an array
 
 	public static void main(String[] args) {
-// will have the menu for users and scanner
-	}
+		// will have the menu for users and scanner
+		// is the only static method in this entire project
+		FoodTruckApp app = new FoodTruckApp(); // Instantiating the FoodTruckApp
+		app.run();
+		Scanner sc = new Scanner(System.in); // adding scanner for user input
+		String foodTruckName = null;
+		String foodTruckMenu = null;
+		int foodTruckRating = 0;
+			//for (int i = 0; i <= fleetOfFoodTrucks; i++) {
 
+				System.out.println("Please input FoodTruck Name: ");
+				foodTruckName = sc.next();
+				
+
+				System.out.println("What type of food does this FoodTruck sell?");
+				foodTruckMenu = sc.next();
+
+				System.out.println("Please rate this FoodTruck on a scale of 1-5.");
+				foodTruckRating = sc.nextInt();
+
+			}
+		
+		
+
+
+
+	public void run() {
+		// call the non-static methods in the FoodTruckApp class
+	}
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,8 +1,62 @@
 package com.skilldistillery.foodtruck.entities;
+
 // does not have a main
 public class FoodTruck {
-	//You will define a FoodTruck class with fields for a 
-	//unique numeric id, a name ("TacoRific", "Mediterranean Medic", etc.), 
-	//food type ("Tacos", "Falafel", etc.), and a numeric rating.
-}
+	// You will define a FoodTruck class with fields for a
+	// unique numeric id, a name ("TacoRific", "Mediterranean Medic", etc.),
+	// food type ("Tacos", "Falafel", etc.), and a numeric rating.
+	private String foodTruckName; // every truck has a name
+	private String foodTruckId; // every truck has an id
+	private String foodTruckMenu; // every truck has a menu
+	private int foodTruckRating; // every truck has a rating
+
+	public FoodTruck() { // creating the no arc constructor (default constructor)
+
+	}
+
+	public FoodTruck(String foodTruckName, String foodTruckId, String foodTruckMenu, int foodTruckRating) { // entering
+																											// them in
+																											// order
+		this.foodTruckName = foodTruckName; // finishing the constructor
+		this.foodTruckId = foodTruckId;
+		this.foodTruckMenu = foodTruckMenu;
+		this.foodTruckRating = foodTruckRating;
+
 //have an array of up to five food trucks.
+
+	}
+
+	public String getFoodTruckName() { // used command, opt, s to generate getters and setters. Seriously. Don't type
+										// them all in again. Save yourself some time.
+		return foodTruckName;
+	}
+
+	public void setFoodTruckName(String foodTruckName) {
+		this.foodTruckName = foodTruckName;
+	}
+
+	public String getFoodTruckId() {
+		return foodTruckId;
+	}
+
+	public void setFoodTruckId(String foodTruckId) {
+		this.foodTruckId = foodTruckId;
+	}
+
+	public String getFoodTruckMenu() {
+		return foodTruckMenu;
+	}
+
+	public void setFoodTruckMenu(String foodTruckMenu) {
+		this.foodTruckMenu = foodTruckMenu;
+	}
+
+	public int getFoodTruckRating() {
+		return foodTruckRating;
+	}
+
+	public void setFoodTruckRating(int foodTruckRating) {
+		this.foodTruckRating = foodTruckRating;
+	}
+
+}
